fd99cec4a881560e04b241b9fb070ec034d37332
Chong Kai <chongkai96@gmail.com>
2022-11-13 20:01:55 +0800

    Setting up files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
+import About from "./components/about"
+import Contact from "./components/contact"
+import Experience from "./components/experience"
+import Footer from "./components/footer"
+import Header from "./components/header"
+import Nav from "./components/nav"
 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
