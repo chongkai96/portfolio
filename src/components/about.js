d53f17183c1f6cd0dee43d5861db986b86a867a9
Tan Chong Kai <81542431+chongkai96@users.noreply.github.com>
2022-12-07 17:01:39 +0800

    about

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,30 @@
 import React from "react";
+import {HiOutlineMagnifyingGlass} from "react-icons/hi2"
+import {GiNotebook} from "react-icons/gi"
 
 const About = () => {
   return (
-    <header className="App-header">
-    </header>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about_card">
+            <HiOutlineMagnifyingGlass/>
+            <h5>Detail-Oriented</h5>
+          </article>
+          <article className="about_card">
+            <HiOutlineMagnifyingGlass/>
+            <h5>Positive Mindset</h5>
+          </article>
+          <article className="about_card">
+            <GiNotebook/>
+            <h5>Desire To Learn</h5>
+          </article>
+        </div>
+      </div>
+    </section>
   );
 }
 
